6f58eae6e13b51a19f7a7558a5ce1d27414ccca6
chaminduthiranjaya <chamindu.cta@gmail.com>
2025-11-24 21:23:33 +0530

    updated tsconfig and layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Geist } from "next/font/google";
+import localFont from "next/font/local";
+import "../styles/globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const impact = localFont({
+  variable: "--font-impact",
+  src: "../../public/fonts/impact.ttf",
 });
 
 export const metadata: Metadata = {
@@ -24,9 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${impact.variable} antialiased`}>
         {children}
       </body>
     </html>
