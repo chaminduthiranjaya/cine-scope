8f16fa9df50ee17db1fc02c7fa1659f019edc8fd
chaminduthiranjaya <chamindu.cta@gmail.com>
2025-12-05 14:09:59 +0530

    added middleware to protect routes

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,9 +1,6 @@
-import { authOptions } from "@/lib/auth/options";
 import Footer from "@/lib/components/layout/Footer";
 import { Header } from "@/lib/components/layout/Header";
 import { QueryProvider } from "@/lib/providers/QueryProvider";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
 import type { ReactNode } from "react";
 
 export default async function ProtectedLayout({
@@ -11,10 +8,6 @@ export default async function ProtectedLayout({
 }: {
   children: ReactNode;
 }) {
-  const session = await getServerSession(authOptions);
-  if (!session) {
-    redirect("/login");
-  }
   return (
     <>
       <Header />
---
+++ src/middleware.ts
@@ -0,0 +1,32 @@
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+const PROTECTED_ROUTES = ["/explore"];
+
+export async function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+  const token = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
+  const isProtected = PROTECTED_ROUTES.some((route) =>
+    pathname.startsWith(route)
+  );
+
+  if (isProtected && !token) {
+    const loginUrl = new URL("/login", req.url);
+    return NextResponse.redirect(loginUrl);
+  }
+
+  if (pathname === "/login" || (pathname === "/register" && token)) {
+    const redirectUrl = new URL("/", req.url);
+    return NextResponse.redirect(redirectUrl);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/login", "/register", "/explore"],
+};
